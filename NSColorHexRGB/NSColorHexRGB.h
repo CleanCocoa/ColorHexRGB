590230764a1a92e653f29e6c5c73c0af3513eb92
Christian Tietze <christian.tietze@gmail.com>
2018-12-20 20:56:04 +0100

    Initial Commit

---
+++ NSColorHexRGB/NSColorHexRGB.h
@@ -0,0 +1,19 @@
+//
+//  NSColorHexRGB.h
+//  NSColorHexRGB
+//
+//  Created by Christian on 20.12.18.
+//  Copyright © 2018 Christian Tietze. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NSColorHexRGB.
+FOUNDATION_EXPORT double NSColorHexRGBVersionNumber;
+
+//! Project version string for NSColorHexRGB.
+FOUNDATION_EXPORT const unsigned char NSColorHexRGBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSColorHexRGB/PublicHeader.h>
+
+
