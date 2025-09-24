fea64593e07008d403bfb07a6e2b7ccd3fd74053
Christian Tietze <me@christiantietze.de>
2025-09-24 07:25:49 +0200

    bump to Swift 6

--- Package.swift
+++ Package.swift
@@ -1,23 +1,23 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 6.2
 
 import PackageDescription
 
 let package = Package(
-  name: "ColorHexRGB",
-  platforms: [
-    .macOS("10.13"),
-  ],
-  products: [
-    .library(
-      name: "ColorHexRGB",
-      targets: ["ColorHexRGB"]),
-  ],
-  targets: [
-    .target(
-      name: "ColorHexRGB",
-      dependencies: []),
-    .testTarget(
-      name: "ColorHexRGBTests",
-      dependencies: ["ColorHexRGB"]),
-  ]
+    name: "ColorHexRGB",
+    platforms: [
+        .macOS("10.13"),
+    ],
+    products: [
+        .library(
+            name: "ColorHexRGB",
+            targets: ["ColorHexRGB"]),
+    ],
+    targets: [
+        .target(
+            name: "ColorHexRGB",
+            dependencies: []),
+        .testTarget(
+            name: "ColorHexRGBTests",
+            dependencies: ["ColorHexRGB"]),
+    ]
 )
